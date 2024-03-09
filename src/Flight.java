7d2c50e98a80d8dafeb20186718774f4c9af4345
Flwa2 <161247348+Flwa2@users.noreply.github.com>
2024-03-10 02:33:02 +0300

    updated

--- src/Flight.java
+++ src/Flight.java
@@ -3,57 +3,66 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
-
 public class Flight {
-    	private int FlightNumber ;
-	private String ArrivalCity;
-	private String DepartureCity;
-	private String Arrival_time;
-	private String Departure_time;
-	private Seat [] listOfSeats;
-	public int nOs;
-	
-	public Flight(int flightNumber, String arrivalCity, String departureCity, String arrival_time,
-			String departure_time , int size) {
-		FlightNumber = flightNumber;
-		ArrivalCity = arrivalCity;
-		DepartureCity = departureCity;
-		Arrival_time = arrival_time;
-		Departure_time = departure_time;
-		listOfSeats = new Seat[size];
-		nOs =0;
-	}
-	
-	public boolean AddSeat ( Seat s)
-	{
-		if ( nOf < listOfSeats.length )
-		{
-			listOfSeats[nOs++] = s;
-		return true;
-		}
-		return false;
-	}
-	public boolean checkAvailbility()
-	{
-		if (nOs < listOfSeats.length)
-			return true;
-		else
-			return false;
-	}
-	
-	public int Capacity()
-	{
-		return listOfSeats.length;
-	}
 
-	@Override
-	public String toString() {
-		String str =  "Flight [FlightNumber=" + FlightNumber + ", ArrivalCity=" + ArrivalCity + ", DepartureCity="
-				+ DepartureCity + ", Arrival_time=" + Arrival_time + ", Departure_time=" + Departure_time + ", nOs="
-				+ nOs + "]";
-		
-		for (int i=0; i< listOfSeats ; i++)
-		str = str + listOfSeats[i].toString();
-	}
-    
+    private int FlightNumber;
+    private String ArrivalCity;
+    private String DepartureCity;
+    private String Arrival_time;
+    private String Departure_time;
+    private Seat[] listOfSeats;
+    public int nOs;
+
+    public Flight(int flightNumber, String arrivalCity, String departureCity, String arrival_time,
+            String departure_time, int size) {
+        FlightNumber = flightNumber;
+        ArrivalCity = arrivalCity;
+        DepartureCity = departureCity;
+        Arrival_time = arrival_time;
+        Departure_time = departure_time;
+        listOfSeats = new Seat[size];
+        nOs = 0;
+    }
+
+    public boolean AddSeat(Seat s) {
+        if (nOs < listOfSeats.length) {
+            listOfSeats[nOs++] = s;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkAvailbility() {
+        if (nOs < listOfSeats.length) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int Capacity() {
+        return listOfSeats.length;
+    }
+
+    public String getArrivalCity() {
+        return ArrivalCity;
+    }
+
+    public String getDepartureCity() {
+        return DepartureCity;
+    }
+
+    @Override
+    public String toString() {
+        String str = "Flight FlightNumber : " + FlightNumber 
+                + "\nArrival City : " + ArrivalCity + "\nDepartureCity : "
+                + DepartureCity + "\nArrival time : " + Arrival_time + "\nDeparture_time : " + Departure_time
+                + " \n number Of seat : " + nOs;
+
+        for (Seat listOfSeat : listOfSeats) {
+            str = str + listOfSeat.toString();
+        }
+        return null;
+    }
+
 }
