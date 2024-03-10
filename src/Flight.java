e248dac53f23ab853cec0c94be23ed3f095b2df8
Shouu112 <162643806+Shouu112@users.noreply.github.com>
2024-03-10 03:49:22 +0300

    Update Flight.java

--- src/Flight.java
+++ src/Flight.java
@@ -24,7 +24,7 @@ public class Flight {
         nOs = 0;
     }
 
-    public boolean AddSeat(Seat s) {
+    public boolean AddSeat(Seat s) { //To add the given seat to the array in the first empty location and return true. If there is no space for the addition return false.
         if (nOs < listOfSeats.length) {
             listOfSeats[nOs++] = s;
             return true;
@@ -32,7 +32,7 @@ public class Flight {
         return false;
     }
 
-    public boolean checkAvailbility() {
+    public boolean checkAvailbility() { // return true if there is seat available, false if there is not seat available.
         if (nOs < listOfSeats.length) {
             return true;
         } else {
@@ -40,20 +40,20 @@ public class Flight {
         }
     }
 
-    public int Capacity() {
+    public int Capacity() { //To return number of seats in the flight.
         return listOfSeats.length;
     }
 
-    public String getArrivalCity() {
+    public String getArrivalCity() { //To return Arrival city.
         return ArrivalCity;
     }
 
-    public String getDepartureCity() {
-        return DepartureCity;
+    public String getDepartureCity() { // To return Departure city.
+        return DepartureCity; 
     }
 
     @Override
-    public String toString() {
+    public String toString() { // 
         String str = "Flight FlightNumber : " + FlightNumber 
                 + "\nArrival City : " + ArrivalCity + "\nDepartureCity : "
                 + DepartureCity + "\nArrival time : " + Arrival_time + "\nDeparture_time : " + Departure_time
