0a8a07544fb76d648cbcaccba6e11db4a010af52
Amani-E8 <162720755+Amani-E8@users.noreply.github.com>
2024-03-16 04:53:33 +0300

    Update Airline.java

--- src/Airline.java
+++ src/Airline.java
@@ -65,4 +65,11 @@ public class Airline {
         }
 
     }
+     public Flight[] getFlights() {
+    Flight[] allFlights = new Flight[ nOf];
+    for (int i = 0; i <  nOf; i++) {
+        allFlights[i] = flights[i];
+    }
+    return allFlights;
+}
 }
