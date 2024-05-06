9c5c33a56538013be165a147e274dcbd726a51c2
Amani-E8 <162720755+Amani-E8@users.noreply.github.com>
2024-05-06 21:00:22 +0300

    Update Airline.java

--- src/Airline.java
+++ src/Airline.java
@@ -54,4 +54,43 @@ public class Airline {
         }
         return false;
     }
-}
+    public void save (String fileName){
+        try{
+        File out =new File (fileName);
+        FileOutputStream fos =new FileOutputStream (out);
+        
+        ObjectOutputStream file =new  ObjectOutputStream (fos);
+        
+        file.writeInt(nOf);
+        file.writeObject(name);
+        
+        for (int i=0; i<nOf;i++)
+            file.writeObject(flights[i]);
+        file.close();
+        JOptionPane.showMessageDialog(null,"Done saving");
+        }  catch(IOException e ){
+            JOptionPane.showMessageDialog(null,"error"+e.toString()); 
+            
+        }
+    }
+    public void loud (String fileName){
+        try{
+        File f =new File (fileName); 
+        FileInputStream fi = new FileInputStream (f);
+          ObjectInputStream in =new  ObjectInputStream (fi);
+          int size =in.readInt();
+          String Name=(String) in.readObject();
+          name=Name;
+          for(int i=0;i<size;i++){
+            Flight obj=(Flight)in.readObject();
+         // this.AddFlights(obj);
+            in.close();
+          }}
+          catch(ClassNotFoundException e){
+            JOptionPane.showMessageDialog(null,"Error reading");      
+                  }
+         catch(IOException e){
+            JOptionPane.showMessageDialog(null,"Error "+ e.toString() );      
+        }
+}}
+
