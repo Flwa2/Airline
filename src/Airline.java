c77be5afd6f9df4dafa6644336b599193d6ac44d
Amani-E8 <162720755+Amani-E8@users.noreply.github.com>
2024-05-06 21:00:53 +0300

    Update Airline.java

--- src/Airline.java
+++ src/Airline.java
@@ -2,6 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+import java.io.*;
+import javax.swing.JOptionPane;
 
 public class Airline {
 
